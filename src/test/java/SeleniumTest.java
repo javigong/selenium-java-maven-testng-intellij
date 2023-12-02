89e50311f6b8de53620c0e0b12a2485f0ee7f009
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-12-01 20:14:38 -0800

    Add chromedriver resource and chrome options

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,8 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
     @Test
     void Setup(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
 
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.selenium.dev");
+        ;
     }
 }
