a684ae979b99640937dfe8c1512fd0dd2de5562d
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-12-06 20:01:27 -0800

    Add beforeTest setup

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,17 +1,24 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
-    @Test
+    public static ChromeOptions options = new ChromeOptions();
+    public static WebDriver driver = new ChromeDriver(options);
+    @BeforeTest
     void Setup(){
-        ChromeOptions options = new ChromeOptions();
+
         options.addArguments("--remote-allow-origins=*");
 
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
-        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.selenium.dev");
+    }
+
+    @Test
+    void testSteps(){
+
     }
 }
